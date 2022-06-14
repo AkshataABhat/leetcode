874a6821503b884d4e97a6ac1dbea5cfc1d3a8b0
Akshata Bhat <74967139+AkshataABhat@users.noreply.github.com>
2022-06-14 10:36:10 +0530

    Time: 149 ms (58.57%), Space: 15.5 MB (6.77%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,17 +1,11 @@
 class Solution(object):
     def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        
         n=len(nums)
-        visited=set()
         
-        for num in nums:
-            visited.add(num)
+        visited=set(nums)
         
-        for i in range(0,n+1):
-            if i not in visited:
-                return i
-        
+        for num in range(0,n+1):
+            if num not in visited:        
+                 return num
         
