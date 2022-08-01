71fcf2863b87a8da3325daaa85a5d3867f10880c
Akshata Bhat <74967139+AkshataABhat@users.noreply.github.com>
2022-08-01 15:16:19 +0530

    Time: 55 ms (38.39%), Space: 13.5 MB (39.71%) - LeetHub

--- 38-count-and-say/38-count-and-say.py
+++ 38-count-and-say/38-count-and-say.py
@@ -8,12 +8,12 @@ class Solution(object):
             return "1"
         prev=self.countAndSay(n-1)
         res=""
-        ctr=1
+        ct=1
         for i in range(len(prev)):
-            if i==len(prev)-1 or prev[i] !=prev[i+1]:
-                res+=str(ctr)+prev[i]
-                ctr=1
+            if i==len(prev)-1 or prev[i]!=prev[i+1]:
+                res+=str(ct)+prev[i]
+                ct=1
             else:
-                ctr+=1
+                ct+=1
         return res
-                
+        
